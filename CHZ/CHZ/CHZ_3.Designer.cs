b38c3f65a522071b52aa10f00c89cb5a95f8bb1e
swellz <d89645603777@gmail.com>
2023-07-27 15:44:37 +0300

    first commit

---
+++ CHZ/CHZ/CHZ_3.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CHZ
+{
+    partial class CHZ_3
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCode = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelCode
+            // 
+            this.labelCode.AutoSize = true;
+            this.labelCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCode.Location = new System.Drawing.Point(164, 15);
+            this.labelCode.Name = "labelCode";
+            this.labelCode.Size = new System.Drawing.Size(89, 18);
+            this.labelCode.TabIndex = 21;
+            this.labelCode.Text = "Кодировка:";
+            this.labelCode.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "UTF-8",
+            "windows-1251"});
+            this.comboBox1.Location = new System.Drawing.Point(259, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 20;
+            this.comboBox1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(143, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Есть русские символы";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Выберите файл:";
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Location = new System.Drawing.Point(136, 87);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(91, 23);
+            this.OkBtn.TabIndex = 16;
+            this.OkBtn.Text = "Готово";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(289, 59);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(91, 23);
+            this.browseBtn.TabIndex = 17;
+            this.browseBtn.Text = "Обзор";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.filePathBox.Location = new System.Drawing.Point(32, 59);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.Size = new System.Drawing.Size(251, 22);
+            this.filePathBox.TabIndex = 15;
+            // 
+            // CHZ_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 114);
+            this.Controls.Add(this.labelCode);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.filePathBox);
+            this.Name = "CHZ_3";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CHZ_3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCode;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.TextBox filePathBox;
+    }
+}
+
